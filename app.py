81a4c7f229ced63c79aa32e53209ecd3a44855b4
David Carboni <david.carboni@ros.gov.uk>
2017-08-15 16:07:35 +0100

    Fixed logging.

--- app.py
+++ app.py
@@ -3,36 +3,40 @@ import sleuth
 
 from flask import Flask
 import requests
+import os
 
 import b3
 
 
 app = Flask("Service-a")
+log = logging.getLogger(app.name)
+log.setLevel(logging.INFO)
+
+port = int(os.getenv("PORT", "8001"))
+service_b = os.getenv("SERVICE_B", "http://localhost:8002/")
 
 
 @app.route('/')
 def service():
-    logger.info(app.name + " has been called.")
+    log.info(app.name + " has been called.")
 
     with b3.SubSpan() as headers:
-        logger.debug("Making a request to service B")
-        r = requests.get("http://localhost:8002/", headers=headers)
-        logger.debug("Service B said: " + str(r.text))
+        log.debug("Making a request to service B")
+        r = requests.get(service_b, headers=headers)
+        log.debug("Service B said: " + str(r.text))
 
     return "Service call succeeded (" + app.name + ")"
 
 
 if __name__ == "__main__":
 
-    logging.getLogger().setLevel(logging.INFO)
-    logger = logging.getLogger(__name__)
-    logger.debug("Starting " + app.name)
+    log.debug("Starting " + app.name)
 
     app.before_request(b3.start_span)
     app.after_request(b3.end_span)
     app.run(
         host="0.0.0.0",
-        port=int(8001),
+        port=int(port),
         debug=True,
         threaded=True
     )
